95899309fe4f5fce9c90a5b4bc2b7202738b90c2
John Comeau <jc@unternet.net>
2025-04-13 19:38:10 -0700

    not doing anything yet.

--- download.py
+++ download.py
@@ -2,13 +2,19 @@
 '''
 download all SRTM3 data from USGS
 '''
-import sys
+import sys, logging  # pylint: disable=multiple-imports
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 WEBSITE = 'https://earthexplorer.usgs.gov/'
 
 def download(url=WEBSITE, pattern='.*'):
-    driver = webdriver.Chrome()
+    options = Options()
+    logging.debug('options: %s', options)
+    options.capabilities['browserName'] = 'chromium'  # not "chrome"
+    logging.debug('options.capabilities: %s', options.capabilities)
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
 
 if __name__ == '__main__':
