0444e6fc87ab94e140fc0b89b0e115d901a0421f
John Comeau <jc@unternet.net>
2025-04-14 14:11:22 -0700

    got rid of hardcoded path

--- download.py
+++ download.py
@@ -3,15 +3,16 @@
 download all SRTM3 data from USGS
 '''
 import sys, logging  # pylint: disable=multiple-imports
+from shutil import which
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 WEBSITE = 'https://earthexplorer.usgs.gov/'
+CHROMEDRIVER = which('chromedriver')
 
 def download(url=WEBSITE, pattern='.*'):
-    service = Service(executable_path='/usr/bin/chromedriver')
+    service = Service(executable_path=CHROMEDRIVER)
     driver = webdriver.Chrome(service=service)
     driver.get(url)
 
