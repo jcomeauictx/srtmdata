f0dc30ce3db9ae9bddc34c197e70c8c3d591292f
John Comeau <jc@unternet.net>
2025-04-14 18:37:22 -0700

    selenium.common.exceptions.InvalidArgumentException: Message: invalid argument: invalid locator; not logged in perhaps?

--- download.py
+++ download.py
@@ -6,6 +6,7 @@ import sys, logging  # pylint: disable=multiple-imports
 from shutil import which
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 WEBSITE = 'https://earthexplorer.usgs.gov/'
@@ -15,6 +16,11 @@ def download(url=WEBSITE, pattern='.*'):
     service = Service(executable_path=CHROMEDRIVER)
     driver = webdriver.Chrome(service=service)
     driver.get(url)
+    click(driver, 'tab2')  # "Data Sets" tab
+
+def click(driver, identifier, idtype=By.ID):
+    element = driver.find_element(identifier, idtype)
+    element.click()
 
 if __name__ == '__main__':
     download(*sys.argv[1:])
