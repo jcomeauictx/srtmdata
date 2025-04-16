ff06a2ad9bb5773451c18b960d6112b40b988bc1
John Comeau <jc@unternet.net>
2025-04-15 20:33:27 -0700

    seems to have found the select box but it dies too fast to be sure

--- download.py
+++ download.py
@@ -34,11 +34,12 @@ def download(url=WEBSITE, pattern='.*_3arc_'):
         find('//label[text()="SRTM Void Filled"]/../../div/input')  # checkbox
     click('//label[text()="SRTM Void Filled"]/../../div/input')  # check box
     click('//div[@id="tab3" and text()="Additional Criteria"]')
-    find('//div[text()="Resolution"]')
-    find('//div[text()="Resolution"]/..')
-    find('//div[text()="Resolution"]/../div[2]')
-    find('//div[text()="Resolution"]/../div[2]//i')
-    click('//div[text()="Resolution"]/../div[2]//i')
+    find('//div/strong[text()="Resolution"]')
+    find('//div/strong[text()="Resolution"]/..')
+    find('//div/strong[text()="Resolution"]/../..')
+    find('//div/strong[text()="Resolution"]/../../div[2]')
+    find('//div/strong[text()="Resolution"]/../../div[2]//i')
+    click('//div/strong[text()="Resolution"]/../../div[2]//i')
     click('//select/option[3][@value="3-ARC"]/..')  # parent "select"
     if '_3arc_' in pattern:
         click('//option[@value="3-ARC"]')
